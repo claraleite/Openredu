987f7399911678c6a3e3254057bba7bbcb901342
claraleite <mccl@cin.ufpe.br>
2023-04-03 22:36:36 -0300

    feature/environment: added list and models to the environments page

---
+++ Openredu/Openredu/EnvironmentTypeModel.swift
@@ -0,0 +1,16 @@
+//
+//  EnvironmentTypeModel.swift
+//  Openredu
+//
+//  Created by mccl on 03/04/23.
+//
+
+import SwiftUI
+
+struct EnvironmentTypeModel: Identifiable {
+    let id = UUID()
+    var title: String
+    var description: String
+}
+
+
