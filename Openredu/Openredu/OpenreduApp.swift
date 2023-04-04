876e39994e391fe7b2b464e337ccc17d7ad9b586
claraleite <mccl@cin.ufpe.br>
2023-04-03 21:07:48 -0300

    adding xcode project

---
+++ Openredu/Openredu/OpenreduApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenreduApp.swift
+//  Openredu
+//
+//  Created by mccl on 03/04/23.
+//
+
+import SwiftUI
+
+@main
+struct OpenreduApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
