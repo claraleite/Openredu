f486d559066471dcf5cf4b51d70b5846b2e5e3eb
claraleite <mccl@cin.ufpe.br>
2023-04-03 21:23:19 -0300

    adding tabview

---
+++ Openredu/Openredu/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  Openredu
+//
+//  Created by mccl on 03/04/23.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile")
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
